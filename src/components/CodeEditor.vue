ca783b49d943aaf798f4fff12b5ff706a9a7391e
zhengjy9 <zhengjynicolas@hotmail.com>
2023-11-14 00:18:59 +0800

    update

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+// const emit=defineEmits(['input'])
+defineProps({
+  label: String,
+  code: String,
+})
+// const updateCode=()=>{
+//   emit('input', code);
+// }
+</script>
+<template>
+  <div>
+    <label>{{ label }}</label>
+    <textarea v-model="code"/>
+  </div>
+</template>
