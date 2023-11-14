b513e033b56104e63593d7425d2e56c63f9db91c
zhengjy9 <zhengjynicolas@hotmail.com>
2023-11-14 11:29:00 +0800

    update

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import SourceDiff from './components/SourceDiff.vue';
+import type { App } from 'vue';
+import type {ComponentOptions} from './types';
+
+declare module '@vue/runtime-core' {
+    export interface GlobalComponents {
+        SourceDiff: typeof SourceDiff;
+    }
+}
+
+export default {
+    install: (app: App, options = {}) => {
+        const { componentName = 'SourceDiff' } = options as ComponentOptions;
+
+        app.component(componentName, SourceDiff);
+    },
+};
+
+export { SourceDiff }
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,46 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'node:path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { terser } from 'rollup-plugin-terser'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: 'source-diff',
+      fileName: format => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+        assetFileNames: assetInfo => {
+          if (assetInfo.name === 'style.css') return 'index.css';
+          return assetInfo.name;
+        },
+      },
+      plugins: [
+        terser({
+          compress: {
+            defaults: false,
+            drop_console: true,
+          },
+        }),
+      ],
+    },
+  },
+  test: {
+    environment: 'jsdom',
+  },
   plugins: [
     vue(),
+    dts()
   ],
   resolve: {
     alias: {
